ee474b21088269bfa5cb27f90b12c11a40c88a61
lld <lld@llddeMac-mini.local>
2025-06-05 16:49:05 +0400

    增加自动下注测试用例

--- testing/ws_test.go
+++ testing/ws_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"go-zrbc/view"
 	"log"
 	"os"
 	"os/signal"
@@ -55,13 +56,6 @@ func TestYMZR_ws_15109(t *testing.T) {
 			}
 		}
 	}
-}
-
-type WsData struct {
-	// protocol协议号
-	Protocol int64 `json:"protocol"`
-	// data
-	Data interface{} `json:"data"`
 }
 
 type AuthResp struct {
@@ -131,7 +125,7 @@ func connect_ws_15101(ch chan string) {
 			if err != nil {
 				log.Fatal("Error reading due to ", err)
 			} else {
-				var wsData WsData
+				var wsData view.WsResp
 				err = json.Unmarshal(message, &wsData)
 				if err != nil {
 					log.Fatal("Error Unmarshal to ", err)
@@ -194,7 +188,234 @@ func TestYMZR_ws_all(t *testing.T) {
 				// if err != nil {
 				// 	log.Fatal("Error Marshal to ", err)
 				// }
-				var wsData WsData
+				var wsData view.WsResp
+				err = json.Unmarshal(message, &wsData)
+				if err != nil {
+					log.Fatal("Error Unmarshal to ", err)
+				}
+				// log.Printf("recv: %v", wsData)
+				if wsData.Protocol == 0 {
+					cc, err := json.Marshal(wsData.Data)
+					if err != nil {
+						log.Fatal("Error Marshal to ", err)
+					}
+					var authResp AuthResp
+					err = json.Unmarshal(cc, &authResp)
+					if err != nil {
+						log.Fatal("Error Unmarshal to ", err)
+					}
+					log.Printf("15109登录成功, sid: %s", authResp.Sid)
+					ch <- authResp.Sid
+					ticker.Stop()
+					break
+				}
+			}
+		}
+	}
+}
+
+func exec_ws_betting_15101(chbetInfo <-chan view.WsBettingCh) {
+	for msg := range chbetInfo {
+		time.Sleep(1 * time.Second)
+
+		wsReq, err := json.Marshal(view.WsReq{
+			Protocol: 22,
+			Data:     msg.BetCh,
+		})
+		if err != nil {
+			log.Fatal("Error Marshal to ", err)
+		}
+		err = msg.Conn.WriteMessage(websocket.TextMessage, wsReq)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("15101 bet success: %s", wsReq)
+	}
+}
+
+func connect_ws_betting_15101(ch chan string) {
+	sid := <-ch
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	betCh := make(chan view.WsBettingCh)
+	defer func() {
+		close(betCh)
+	}()
+	go exec_ws_betting_15101(betCh)
+
+	c, _, err := websocket.DefaultDialer.Dial("wss://59f7-37-157-223-27.ngrok-free.app/15101", nil)
+	if err != nil {
+		log.Fatal("dial 15101:", err)
+	}
+	defer c.Close()
+	log.Printf("connect 15101 success!")
+
+	jsonData := []byte(`{"protocol":1,"data":{"dtBetLimitSelectID":{"101":124,"102":125,"103":9,"104":126,"105":127,"106":128,"107":129,"108":149,"110":131,"111":150,"112":250,"113":251,"117":260,"121":261,"125":600,"126":599,"128":584,"129":602,"301":29},"bGroupList":false,"videoName":"TC","videoDelay":3000,"userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"}}`)
+
+	// Step 1: Parse the JSON into a map
+	var jsonMap map[string]interface{}
+	if err := json.Unmarshal(jsonData, &jsonMap); err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	// Step 2: Add the new field to the "data" object
+	if data, ok := jsonMap["data"].(map[string]interface{}); ok {
+		data["sid"] = sid
+	} else {
+		log.Fatalf("Failed to access 'data' field")
+	}
+
+	// Step 3: Convert back to []byte
+	updatedJSON, err := json.Marshal(jsonMap)
+	if err != nil {
+		log.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	err = c.WriteMessage(websocket.TextMessage, updatedJSON)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ticker := time.NewTicker(5 * time.Second) // send a unsolicited pong frame every 5 seconds
+	// periodicMsgTicker := time.NewTicker(2 * time.Second) // send a betting frame every 25 seconds
+	begin_time := time.Now()
+	defer func() {
+		ticker.Stop()
+	}()
+
+	recvBetFlag := true
+	firstTwentyOneFlag := true
+	joinTableFlag := false
+	betSerialNumber := 1
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			return
+		case <-ticker.C:
+			log.Printf("%v sending a unsolicited ping frame, has been running for %v seconds", time.Now().Format(time.RFC3339), time.Since(begin_time).Seconds())
+			err = c.WriteMessage(websocket.PingMessage, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		default:
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Fatal("Error reading due to ", err)
+			} else {
+				var wsData view.WsResp
+				err = json.Unmarshal(message, &wsData)
+				if err != nil {
+					log.Fatal("Error Unmarshal to ", err)
+				}
+				if wsData.Protocol == 0 {
+					log.Printf("15101 登录成功: %s", message)
+				} else if wsData.Protocol == 22 {
+					log.Printf("15101 投注成功回复: %s", message)
+					recvBetFlag = true
+				} else if wsData.Protocol == 25 {
+					log.Printf("15101 得到一局结果: %s", message)
+				} else if wsData.Protocol == 31 {
+					log.Printf("15101 派彩成功: %s", message)
+				} else if wsData.Protocol == 10 {
+					log.Printf("15101 进入桌台成功: %s", message)
+					joinTableFlag = true
+				} else if wsData.Protocol == 38 {
+					log.Printf("15101 得到下注时间剩余秒数: %s", message)
+					var wsBetTimeResp view.WsBetTimeResp
+					err = json.Unmarshal(message, &wsBetTimeResp)
+					if err != nil {
+						log.Fatal("Error Unmarshal to ", err)
+					}
+					if wsBetTimeResp.Data.GroupID != 3 {
+						continue
+					}
+					if firstTwentyOneFlag {
+						firstTwentyOneFlag = false
+						err = c.WriteMessage(websocket.TextMessage, []byte(`{"protocol":10,"data":{"dtBetLimitSelectID":{"101":124,"102":125,"103":9,"104":126,"105":127,"106":128,"107":129,"108":149,"110":131,"111":150,"112":250,"113":251,"117":260,"121":261,"125":600,"126":599,"128":584,"129":602,"301":29},"groupID":3}}`))
+						if err != nil {
+							log.Fatal(err)
+						}
+						log.Printf("15101 join table success")
+						log.Printf("15101 得到下注时间剩余秒数信息第一次: %v", wsBetTimeResp)
+						continue
+					}
+					log.Printf("15101 得到下注时间剩余秒数: %v", wsBetTimeResp)
+					if recvBetFlag && joinTableFlag {
+						var wsBettingData = view.WsBettingData{
+							BetSerialNumber: betSerialNumber,
+							GameNo:          wsBetTimeResp.Data.GameNo,
+							GameNoRound:     wsBetTimeResp.Data.GameNoRound,
+							BetArr: []view.WsBettingInfoItem{
+								{
+									BetArea:     1,
+									AddBetMoney: 100,
+								},
+							},
+							Commission: 0,
+						}
+						betCh <- view.WsBettingCh{
+							Conn:  c,
+							BetCh: wsBettingData,
+						}
+						recvBetFlag = false
+						betSerialNumber++
+					}
+				} else {
+					log.Printf("15101 recv: %s", "other msg")
+				}
+			}
+		}
+	}
+}
+
+func TestYMZR_ws_betting_all(t *testing.T) {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	c, _, err := websocket.DefaultDialer.Dial("wss://59f7-37-157-223-27.ngrok-free.app/15109", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+	log.Printf("connect 15109 success!")
+
+	err = c.WriteMessage(websocket.TextMessage, []byte(`{"protocol":0,"data":{"account":"laugh","password":"123456","dtBetLimitSelectID":{},"bGroupList":false,"videoName":"TC","videoDelay":3000,"userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"}}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 创建一个字符串类型的通道
+	ch := make(chan string)
+	go connect_ws_betting_15101(ch)
+
+	ticker := time.NewTicker(5 * time.Second) // send a unsolicited pong frame every 15 seconds
+	begin_time := time.Now()
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			log.Printf("%v sending a unsolicited ping frame, has been running for %v seconds", time.Now().Format(time.RFC3339), time.Since(begin_time).Seconds())
+			err = c.WriteMessage(websocket.PingMessage, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		default:
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Fatal("Error reading due to ", err)
+			} else {
+				// txt := string(message)
+				// log.Printf("recv: %s", txt)
+				// bb, err := json.Marshal(message)
+				// if err != nil {
+				// 	log.Fatal("Error Marshal to ", err)
+				// }
+				var wsData view.WsResp
 				err = json.Unmarshal(message, &wsData)
 				if err != nil {
 					log.Fatal("Error Unmarshal to ", err)
--- view/ys_channel.go
+++ view/ys_channel.go
@@ -1,5 +1,7 @@
 package view
 
+import "github.com/gorilla/websocket"
+
 type WsReq struct {
 	Protocol int         `json:"protocol"`
 	Data     interface{} `json:"data"`
@@ -28,4 +30,91 @@ type AuthResp struct {
 type WsUser struct {
 	// id
 	ID int64 `json:"id" description:"会员id"`
+}
+
+type WsBettingInfoItem struct {
+	BetArea     int `json:"betArea"`
+	AddBetMoney int `json:"addBetMoney"`
+}
+
+type WsBettingData struct {
+	BetSerialNumber int                 `json:"betSerialNumber"`
+	GameNo          int                 `json:"gameNo"`
+	GameNoRound     int                 `json:"gameNoRound"`
+	BetArr          []WsBettingInfoItem `json:"betArr"`
+	Commission      int                 `json:"commission"`
+}
+
+type TableDtExtend struct {
+	NetGroupName   string `json:"netGroupName"`
+	PhoneGroupName string `json:"phoneGroupName"`
+	TableName      string `json:"tableName"`
+	NetType        string `json:"netType"`
+	PhoneType      string `json:"phoneType"`
+}
+type WsTableData struct {
+	GameID                   int           `json:"gameID"`
+	GroupID                  int           `json:"groupID"`
+	GroupType                int           `json:"groupType"`
+	GameNo                   int           `json:"gameNo"`
+	GameNoRound              int           `json:"gameNoRound"`
+	DealerID                 int           `json:"dealerID"`
+	DealerName               string        `json:"dealerName"`
+	DealerImage              string        `json:"dealerImage"`
+	DealerImage2             string        `json:"dealerImage2"`
+	Dealer2ID                int           `json:"dealer2ID"`
+	Dealer2Name              string        `json:"dealer2Name"`
+	Dealer2Image             string        `json:"dealer2Image"`
+	Dealer2Image2            string        `json:"dealer2Image2"`
+	BetMilliSecond           int           `json:"betMilliSecond"`
+	BWantToShuffle           bool          `json:"bWantToShuffle"`
+	BWantToEnd               bool          `json:"bWantToEnd"`
+	KeyStatus                int           `json:"keyStatus"`
+	GameMode                 int           `json:"gameMode"`
+	SingleLimit              int           `json:"singleLimit"`
+	TableMinBet              int           `json:"tableMinBet"`
+	TableMaxBet              int           `json:"tableMaxBet"`
+	TableTieMinBet           int           `json:"tableTieMinBet"`
+	TableTieMaxBet           int           `json:"tableTieMaxBet"`
+	TablePairMinBet          int           `json:"tablePairMinBet"`
+	TablePairMaxBet          int           `json:"tablePairMaxBet"`
+	TableType                int           `json:"tableType"`
+	TableStatus              int           `json:"tableStatus"`
+	TableSort                int           `json:"tableSort"`
+	TableSort2               int           `json:"tableSort2"`
+	ReservedTable            int           `json:"reservedTable"`
+	ReservedTableParentIDArr []int         `json:"reservedTableParentIDArr"`
+	ReservedTableMemberIDArr []int         `json:"reservedTableMemberIDArr"`
+	TableDtExtend            TableDtExtend `json:"tableDtExtend"`
+}
+
+type WsTableData21 struct {
+	Protocol int         `json:"protocol"`
+	Data     WsTableData `json:"data"`
+}
+
+type WsJoinTableData struct {
+	DtBetLimitSelectID map[string]int `json:"dtBetLimitSelectID"`
+	GroupID            int            `json:"groupID"`
+}
+
+type WsBettingCh struct {
+	Conn  *websocket.Conn
+	BetCh WsBettingData
+}
+
+type WsBetTimeData struct {
+	GameID          int                    `json:"gameID"`
+	GroupID         int                    `json:"groupID"`
+	GroupType       int                    `json:"groupType"`
+	GameNo          int                    `json:"gameNo"`
+	GameNoRound     int                    `json:"gameNoRound"`
+	BetTimeCount    int                    `json:"betTimeCount"`
+	BetTimeContent  map[string]interface{} `json:"betTimeContent"`
+	TimeMillisecond int                    `json:"timeMillisecond"`
+}
+
+type WsBetTimeResp struct {
+	Protocol int           `json:"protocol"`
+	Data     WsBetTimeData `json:"data"`
 }
