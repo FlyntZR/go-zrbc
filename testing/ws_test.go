a2ce5857792313f60052ec12429321d3cfad52ab
lld <lld@Mac.localdomain>
2025-06-11 19:02:28 +0400

    优化自动化下注程序2

--- testing/ws_test.go
+++ testing/ws_test.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-zrbc/view"
 	"log"
 	"math/rand"
 	"os"
 	"os/signal"
+	"sync"
 	"testing"
 	"time"
 
@@ -161,6 +163,7 @@ func TestYMZR_ws_all(t *testing.T) {
 
 	// 创建一个字符串类型的通道
 	ch := make(chan string)
+	defer close(ch)
 	go connect_ws_15101(ch)
 
 	ticker := time.NewTicker(5 * time.Second) // send a unsolicited pong frame every 15 seconds
@@ -234,7 +237,7 @@ func exec_ws_betting_15101(chbetInfo <-chan view.WsBettingCh) {
 	}
 }
 
-func connect_ws_betting_15101(ch chan string, userName string) {
+func connect_ws_betting_15101(ch chan string) {
 	rand.Seed(time.Now().UnixNano())
 	sid := <-ch
 
@@ -363,45 +366,24 @@ func connect_ws_betting_15101(ch chan string, userName string) {
 					}
 					log.Printf("15101 得到下注时间剩余秒数: %v", wsBetTimeResp)
 					if recvBetResultFlag && modifyBetLimitFlag {
-						if userName == "laugh_g_2" {
-							var wsBettingData = view.WsBettingData{
-								BetSerialNumber: betSerialNumber,
-								GameNo:          wsBetTimeResp.Data.GameNo,
-								GameNoRound:     wsBetTimeResp.Data.GameNoRound,
-								BetArr: []view.WsBettingInfoItem{
-									{
-										BetArea:     2,
-										AddBetMoney: []int{100, 200, 300}[rand.Intn(3)],
-									},
+						var wsBettingData = view.WsBettingData{
+							BetSerialNumber: betSerialNumber,
+							GameNo:          wsBetTimeResp.Data.GameNo,
+							GameNoRound:     wsBetTimeResp.Data.GameNoRound,
+							BetArr: []view.WsBettingInfoItem{
+								{
+									BetArea:     []int{1, 2, 3}[rand.Intn(3)],
+									AddBetMoney: []int{100, 200, 300}[rand.Intn(3)],
 								},
-								Commission: 0,
-							}
-							betCh <- view.WsBettingCh{
-								Conn:  c,
-								BetCh: wsBettingData,
-							}
-							recvBetResultFlag = false
-							betSerialNumber++
-						} else if userName == "laugh" {
-							var wsBettingData = view.WsBettingData{
-								BetSerialNumber: betSerialNumber,
-								GameNo:          wsBetTimeResp.Data.GameNo,
-								GameNoRound:     wsBetTimeResp.Data.GameNoRound,
-								BetArr: []view.WsBettingInfoItem{
-									{
-										BetArea:     1,
-										AddBetMoney: []int{100, 200, 300}[rand.Intn(3)],
-									},
-								},
-								Commission: 0,
-							}
-							betCh <- view.WsBettingCh{
-								Conn:  c,
-								BetCh: wsBettingData,
-							}
-							recvBetResultFlag = false
-							betSerialNumber++
+							},
+							Commission: 0,
 						}
+						betCh <- view.WsBettingCh{
+							Conn:  c,
+							BetCh: wsBettingData,
+						}
+						recvBetResultFlag = false
+						betSerialNumber++
 					}
 				} else {
 					log.Printf("15101 recv: %s", "other msg")
@@ -411,78 +393,96 @@ func connect_ws_betting_15101(ch chan string, userName string) {
 	}
 }
 
-func TestYMZR_ws_betting_all(t *testing.T) {
+// runSingleBettingTest runs a single betting test case with the given account
+func runSingleBettingTest(t *testing.T, account string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	c, _, err := websocket.DefaultDialer.Dial("wss://ffe8-37-157-223-27.ngrok-free.app/15109", nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		log.Printf("dial error for account %s: %v", account, err)
+		return
 	}
 	defer c.Close()
-	log.Printf("connect 15109 success!")
+	log.Printf("connect 15109 success for account: %s!", account)
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(`{"protocol":0,"data":{"account":"laugh","password":"123456","dtBetLimitSelectID":{},"bGroupList":false,"videoName":"TC","videoDelay":3000,"userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"}}`))
+	authMsg := fmt.Sprintf(`{"protocol":0,"data":{"account":"%s","password":"123456","dtBetLimitSelectID":{},"bGroupList":false,"videoName":"TC","videoDelay":3000,"userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"}}`, account)
+	err = c.WriteMessage(websocket.TextMessage, []byte(authMsg))
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("write error for account %s: %v", account, err)
+		return
 	}
 
-	// 创建一个字符串类型的通道
 	ch := make(chan string)
-	go connect_ws_betting_15101(ch, "laugh")
+	defer close(ch)
+	go connect_ws_betting_15101(ch)
 
-	ticker := time.NewTicker(5 * time.Second) // send a unsolicited pong frame every 15 seconds
+	ticker := time.NewTicker(5 * time.Second)
 	begin_time := time.Now()
 	for {
 		select {
 		case <-interrupt:
-			log.Println("interrupt")
+			log.Printf("interrupt for account: %s", account)
 			ticker.Stop()
 			return
 		case <-ticker.C:
-			log.Printf("%v sending a unsolicited ping frame, has been running for %v seconds", time.Now().Format(time.RFC3339), time.Since(begin_time).Seconds())
+			log.Printf("%v sending a unsolicited ping frame for account %s, has been running for %v seconds",
+				time.Now().Format(time.RFC3339), account, time.Since(begin_time).Seconds())
 			err = c.WriteMessage(websocket.PingMessage, nil)
 			if err != nil {
-				log.Fatal(err)
+				log.Printf("ping error for account %s: %v", account, err)
+				return
 			}
 		default:
 			_, message, err := c.ReadMessage()
 			if err != nil {
-				log.Fatal("Error reading due to ", err)
-			} else {
-				// txt := string(message)
-				// log.Printf("recv: %s", txt)
-				// bb, err := json.Marshal(message)
-				// if err != nil {
-				// 	log.Fatal("Error Marshal to ", err)
-				// }
-				var wsData view.WsResp
-				err = json.Unmarshal(message, &wsData)
+				log.Printf("read error for account %s: %v", account, err)
+				return
+			}
+			var wsData view.WsResp
+			err = json.Unmarshal(message, &wsData)
+			if err != nil {
+				log.Printf("unmarshal error for account %s: %v", account, err)
+				return
+			}
+			if wsData.Protocol == 0 {
+				cc, err := json.Marshal(wsData.Data)
 				if err != nil {
-					log.Fatal("Error Unmarshal to ", err)
+					log.Printf("marshal error for account %s: %v", account, err)
+					return
 				}
-				// log.Printf("recv: %v", wsData)
-				if wsData.Protocol == 0 {
-					cc, err := json.Marshal(wsData.Data)
-					if err != nil {
-						log.Fatal("Error Marshal to ", err)
-					}
-					var authResp AuthResp
-					err = json.Unmarshal(cc, &authResp)
-					if err != nil {
-						log.Fatal("Error Unmarshal to ", err)
-					}
-					log.Printf("15109登录成功, sid: %s", authResp.Sid)
-					ch <- authResp.Sid
-					ticker.Stop()
-					break
+				var authResp AuthResp
+				err = json.Unmarshal(cc, &authResp)
+				if err != nil {
+					log.Printf("unmarshal auth resp error for account %s: %v", account, err)
+					return
 				}
+				log.Printf("15109登录成功 for account %s, sid: %s", account, authResp.Sid)
+				ch <- authResp.Sid
+				ticker.Stop()
+				break
 			}
 		}
 	}
 }
 
-func TestYMZR_ws_betting_other(t *testing.T) {
+// TestYMZR_ws_betting_multiple runs multiple concurrent betting test cases
+func TestYMZR_ws_betting_multiple(t *testing.T) {
+	var wg sync.WaitGroup
+	numTests := 10
+
+	for i := 0; i < numTests; i++ {
+		wg.Add(1)
+		account := fmt.Sprintf("laugh_g_%d", i+1)
+		go runSingleBettingTest(t, account, &wg)
+	}
+
+	wg.Wait()
+}
+
+func TestYMZR_ws_betting_bak(t *testing.T) {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
@@ -500,7 +500,8 @@ func TestYMZR_ws_betting_other(t *testing.T) {
 
 	// 创建一个字符串类型的通道
 	ch := make(chan string)
-	go connect_ws_betting_15101(ch, "laugh_g_2")
+	defer close(ch)
+	go connect_ws_betting_15101(ch)
 
 	ticker := time.NewTicker(5 * time.Second) // send a unsolicited pong frame every 15 seconds
 	begin_time := time.Now()
